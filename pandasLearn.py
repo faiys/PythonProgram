2586f49ff0c6ae6caeaed5dc74ac369ddd4c8227
faiys <youtupe761@gmail.com>
2025-07-24 17:44:56 +0530

    Feed:add plot method from pandas

--- pandasLearn.py
+++ pandasLearn.py
@@ -1,6 +1,8 @@
 import sys
 import pandas as pd
 
+import matplotlib.pyplot as plt
+
 class PandasWrapper():
     def __init__(self):
         pass
@@ -10,9 +12,13 @@ class PandasWrapper():
         return pd.__version__
 
     def csv(self):
+        print("\t\t\t **Max Row return only 60 rows default** \t\t\t")
+        print(pd.options.display.max_rows)
         print("\t\t\t **Read CSV** \t\t\t")
-        df = pd.read_csv("Org Users.csv")
-        return df.to_string()
+        pd.options.display.max_rows = 2
+        read_csv_var = pd.read_csv("Org Users.csv")
+        print(read_csv_var.to_string())
+        print(read_csv_var)
 
     def CreateSeries(self):
         print("\t\t\t **one-dimensional array** \t\t\t")
@@ -35,6 +41,44 @@ class PandasWrapper():
         print("\t\t\t **DataFrame use Loc Atttribute** \t\t\t")
         print(myFram.loc["Sno3"])
         print(myFram.loc[["Sno1","Sno2"]])
+
+    def analyzing_date(self):
+        print("\t\t\t **Analyzing Data using head()** \t\t\t")
+        print("\t\t\t **head() defauld 5 row. head(100) show first 100 rows** \t\t\t")
+        hear_var = pd.read_csv("Org Users.csv")
+        print(hear_var.head(2))
+        print("\t\t\t **tail() defauld 5 row. tail(100) show last 100 rows** \t\t\t")
+        print(hear_var.tail(2))
+        print("\t\t\t **info() show details from given csv ** \t\t\t")
+        print(hear_var.info())
+
+    def correlation(self):
+        print("\t\t\t  *correlation corr()* \t\t\t")
+        data = {
+            'A': [1, 2, 3, 4, 5],
+            'B': [5, 4, 3, 2, 1],
+            'C': [2, 2, 3, 4, 4]
+        }
+        MyFrame = pd.DataFrame(data)
+        print(MyFrame)
+        corr_var = MyFrame.corr()
+        print(corr_var)
+    
+    def plots(self):
+        print("\t\t\t  *Plots for visuala Diagram* \t\t\t")
+        data = {
+            'A': [1, 2, 3, 4, 5],
+            'B': [5, 4, 3, 2, 1],
+            'C': [2, 2, 3, 4, 4]
+        }
+        myFrD = pd.DataFrame(data)
+        # Default plot
+        myFrD.plot()
+        # Scatter plot ('line', 'bar', 'barh', 'kde', 'density', 'area', 'hist', 'box', 'pie', 'scatter', 'hexbin')
+        myFr = pd.read_csv("Org Users.csv")
+        # myFr.plot(kind = 'scatter', x="A",y="C")
+        myFr.plot(kind = 'scatter', x='First Name', y = 'User Status')
+        plt.show()
 
 pan_obj = PandasWrapper()
 # methods = [method for method in dir(pan_obj) if callable(getattr(pan_obj, method)) and not method.startswith("__")]
