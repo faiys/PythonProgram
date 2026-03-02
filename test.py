79710381ec98694083140b243acb8a469bceb6fc
faiyasZen <faiyas@zentegra.com>
2026-03-02 21:50:55 +0530

    Mod:Learn LLM for streaming, pydantic validation, structured output

--- test.py
+++ test.py
@@ -1,23 +1,53 @@
 from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel
 
+class UserInfo(BaseModel):
+    name:str
+    email:str
+
+SYSTEMPROMT = """"
+You are an intelligent AI assistant designed to help users with general questions, technical problems, and guidance.
+
+Your goals:
+- Be helpful, accurate, and concise.
+- Prefer practical, actionable answers.
+- For programming questions, provide correct and runnable code.
+- For ambiguous questions, ask a brief clarifying question.
+- If you do not know the answer, clearly say so.
+
+Response style:
+- Friendly but professional tone.
+- Structured when helpful (steps, bullets, code blocks).
+- Avoid unnecessary verbosity.
+- Use the conversation history to maintain context.
+
+Safety:
+- Never invent facts.
+- Avoid unsafe or harmful instructions.
+"""
 
 llm = ChatOllama(
     model="qwen2.5:7b",
     temperature=0.2,
+    streaming=True,
     num_predict=200
 )
 
 promt = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a helpful AI assistant."),
+        ("system", SYSTEMPROMT),
         ("human", "{user_input}")
     ]
 )
-chain = promt | llm
+
+structure_list = llm.with_structured_output(UserInfo)
+chain = promt | structure_list
 resp = chain.invoke(
     {
-        "user_input":"i have 4 mango and 6 apple. what i will eat first?"
+        "user_input":"My name is Faiyas and email is faiyas@test.com"
     }
-    )
-print(resp.content)
+)
+print(resp)
+# for chunk in chain.stream("User name is test@gmail.com. The ID is 123. I want ti json format"):
+#     print(chunk,end="", flush=True)
